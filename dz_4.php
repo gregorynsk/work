88dd6b75f9942e11c2d1fe09f6cfcb00807c1797
Gregory Mihaylovich <gregorybrodskiy@gmail.com>
2015-01-06 22:58:16 +0600

    fix

--- dz_4.php
+++ dz_4.php
@@ -24,21 +24,21 @@ $bd=  parse_ini_string($ini_string, true);
 
 //Перевод в менее раздражающий вид
 
-$bd_s=array(
+$bd_simple=array(
 'bear'=>array(
     'price'=>$bd["игрушка мягкая мишка белый"]["цена"],
     'number'=>$bd["игрушка мягкая мишка белый"]["количество заказано"],
-    'sclad'=>$bd["игрушка мягкая мишка белый"]["осталось на складе"],
+    'storage'=>$bd["игрушка мягкая мишка белый"]["осталось на складе"],
     'diskont'=>$bd["игрушка мягкая мишка белый"]["diskont"]),
 'jacket'=>array(
     'price'=>$bd["одежда детская куртка синяя синтепон"]["цена"],
     'number'=>$bd["одежда детская куртка синяя синтепон"]["количество заказано"],
-    'sclad'=>$bd["одежда детская куртка синяя синтепон"]["осталось на складе"],
+    'storage'=>$bd["одежда детская куртка синяя синтепон"]["осталось на складе"],
     'diskont'=>$bd["одежда детская куртка синяя синтепон"]["diskont"]),
 'bike'=>array(
     'price'=>$bd["игрушка детская велосипед"]["цена"],
     'number'=>$bd["игрушка детская велосипед"]["количество заказано"],
-    'sclad'=>$bd["игрушка детская велосипед"]["осталось на складе"],
+    'storage'=>$bd["игрушка детская велосипед"]["осталось на складе"],
     'diskont'=>$bd["игрушка детская велосипед"]["diskont"]));
       
 echo '<h2>Корзина товаров</h2>';
@@ -47,63 +47,63 @@ echo '<h3>Ваш заказ:</h3>';
 //Мишки
 
 echo 'Товар: Игрушка мягкая мишка белый<br>';
-echo 'Цена за штуку: '.$bd_s["bear"]["price"].' руб.<br>';
-if ($bd_s["bear"]["number"]<=$bd_s["bear"]["sclad"]){
-    echo 'Количество: '.$bd_s["bear"]["number"].' шт.<br>';
-    $bear_n = $bd_s["bear"]["number"];
+echo 'Цена за штуку: '.$bd_simple["bear"]["price"].' руб.<br>';
+if ($bd_simple["bear"]["number"]<=$bd_simple["bear"]["storage"]){
+    echo 'Количество: '.$bd_simple["bear"]["number"].' шт.<br>';
+    $bear__real_number = $bd_simple["bear"]["number"];
 }else {
-    echo '<font color="red">Количество: '.$bd_s["bear"]["number"].' шт. (есть '
-            .$bd_s["bear"]["sclad"].' шт.)</font><br>';
-    $bear_n = $bd_s["bear"]["sclad"];
+    echo '<font color="red">Количество: '.$bd_simple["bear"]["number"].' шт. (есть '
+            .$bd_simple["bear"]["storage"].' шт.)</font><br>';
+    $bear__real_number = $bd_simple["bear"]["storage"];
 }
-echo 'Остаток на складе: '.$bd_s["bear"]["sclad"].' шт.<br><br>';
+echo 'Остаток на складе: '.$bd_simple["bear"]["storage"].' шт.<br><br>';
 
 //Куртки
 
 echo 'Товар: Одежда детская куртка синяя синтепон<br>';
-echo 'Цена за штуку: '.$bd_s["jacket"]["price"].' руб.<br>';
-if ($bd_s["jacket"]["number"]<=$bd_s["jacket"]["sclad"]){
-    echo 'Количество: '.$bd_s["jacket"]["number"].' шт.<br>';
-    $jacket_n = $bd_s["jacket"]["number"];
+echo 'Цена за штуку: '.$bd_simple["jacket"]["price"].' руб.<br>';
+if ($bd_simple["jacket"]["number"]<=$bd_simple["jacket"]["storage"]){
+    echo 'Количество: '.$bd_simple["jacket"]["number"].' шт.<br>';
+    $jacket__real_number = $bd_simple["jacket"]["number"];
 }else {
-    echo '<font color="red">Количество: '.$bd_s["jacket"]["number"].' шт. (есть '
-            .$bd_s["jacket"]["sclad"].' шт.)</font><br>';
-    $jacket_n = $bd_s["jacket"]["sclad"];
+    echo '<font color="red">Количество: '.$bd_simple["jacket"]["number"].' шт. (есть '
+            .$bd_simple["jacket"]["storage"].' шт.)</font><br>';
+    $jacket__real_number = $bd_simple["jacket"]["storage"];
 }
-echo 'Остаток на складе: '.$bd_s["jacket"]["sclad"].' шт.<br><br>';
+echo 'Остаток на складе: '.$bd_simple["jacket"]["storage"].' шт.<br><br>';
 
 //Велики
 
 echo 'Товар: Игрушка детская велосипед<br>';
-echo 'Цена за штуку: '.$bd_s["bike"]["price"].' руб.<br>';
-if ($bd_s["bike"]["number"]<=$bd_s["bike"]["sclad"]){
-    echo 'Количество: '.$bd_s["bike"]["number"].' шт.<br>';
-    $bike_n = $bd_s["bike"]["number"];
+echo 'Цена за штуку: '.$bd_simple["bike"]["price"].' руб.<br>';
+if ($bd_simple["bike"]["number"]<=$bd_simple["bike"]["storage"]){
+    echo 'Количество: '.$bd_simple["bike"]["number"].' шт.<br>';
+    $bike__real_number = $bd_simple["bike"]["number"];
 }else {
-    echo '<font color="red">Количество: '.$bd_s["bike"]["number"].' шт. (есть '
-            .$bd_s["bike"]["sclad"].' шт.)</font><br>';
-    $bike_n = $bd_s["bike"]["sclad"];
+    echo '<font color="red">Количество: '.$bd_simple["bike"]["number"].' шт. (есть '
+            .$bd_simple["bike"]["storage"].' шт.)</font><br>';
+    $bike__real_number = $bd_simple["bike"]["storage"];
 }
-echo 'Остаток на складе: '.$bd_s["bike"]["sclad"].' шт.<br><br>';
+echo 'Остаток на складе: '.$bd_simple["bike"]["storage"].' шт.<br><br>';
 
 //Функции
 
-function sum_n($bd_s) { 
-    global $bear_n, $jacket_n, $bike_n;
-    return $bear_n + $jacket_n + $bike_n;
+function sum_n() { 
+    global $bear__real_number, $jacket__real_number, $bike__real_number;
+    return $bear__real_number + $jacket__real_number + $bike__real_number;
 }
 
-function sum_p($bd_s) {
-    global $bear_n, $jacket_n, $bike_n, $con_b, $con_j, $con_v;
-    $a=($bear_n*$bd_s["bear"]["price"]*$con_b);
-    $b=($jacket_n*$bd_s["jacket"]["price"]*$con_j);
-    if ($bd_s["bike"]["number"]>=3 && $bd_s["bike"]["sclad"]>=3){
-        $c=($bike_n*($bd_s["bike"]["price"]*(0.7+$con_v-1)));
+function sum_p($bd_simple) {
+    global $bear__real_number, $jacket__real_number, $bike__real_number, $con_b, $con_j, $con_v;
+    $bear_price=($bear__real_number*$bd_simple["bear"]["price"]*$con_b);
+    $jacket_price=($jacket__real_number*$bd_simple["jacket"]["price"]*$con_j);
+    if ($bd_simple["bike"]["number"]>=3 && $bd_simple["bike"]["storage"]>=3){
+        $bike_price=($bike__real_number*($bd_simple["bike"]["price"]*(0.7+$con_v-1)));
     }
     else {
-        $c=($bike_n*$bd_s["bike"]["price"]*$con_v);
+        $bike_price=($bike__real_number*$bd_simple["bike"]["price"]*$con_v);
     }
-        return $a+$b+$c;
+        return $bear_price+$jacket_price+$bike_price;
     
 }
 
@@ -150,19 +150,19 @@ function diskont2($param) {
 //Скидки
 
 echo '<h3>Скидки:</h3>';
-if ($bd_s["bike"]["number"]>=3 && $bd_s["bike"]["sclad"]>=3){
+if ($bd_simple["bike"]["number"]>=3 && $bd_simple["bike"]["storage"]>=3){
     echo 'Вы заказали более двух велосипедов, поэтому получаете скидку 30% на эту позицию<br>';
 }
  else {
     echo 'Закажите более двух велосипедов, чтобы получить скидку 30% на эту позицию<br>';
 }
-$func = $bd_s["bear"]["diskont"];
+$func = $bd_simple["bear"]["diskont"];
 $func('bear');
 echo '<br>';
-$func = $bd_s["jacket"]["diskont"];
+$func = $bd_simple["jacket"]["diskont"];
 $func('jacket');
 echo '<br>';
-$func = $bd_s["bike"]["diskont"];
+$func = $bd_simple["bike"]["diskont"];
 $func('bike');
 echo '<br>';
 
@@ -170,38 +170,38 @@ echo '<br>';
 
 echo '<h3>Итого:</h3>';
 
-if ($bd_s["bear"]&&$bd_s["jacket"]&&$bd_s["bike"]){
+if ($bd_simple["bear"]&&$bd_simple["jacket"]&&$bd_simple["bike"]){
     echo 'Количество наименований: 3<br>';
 } 
 
-echo 'Общее количество заказанных товаров: '.sum_n($bd_s).' шт.<br>';
-echo 'Общая сумма заказа: '.sum_p($bd_s).' руб.<br>';
+echo 'Общее количество заказанных товаров: '.sum_n().' шт.<br>';
+echo 'Общая сумма заказа: '.sum_p($bd_simple).' руб.<br>';
 
 //Уведомления
 
 echo '<h3>Уведомления:</h3>';
  
 
-echo ($bd_s["bear"]["number"]<=$bd_s["bear"]["sclad"]
-    && $bd_s["jacket"]["number"]<=$bd_s["jacket"]["sclad"]
-    && $bd_s["bike"]["number"]<=$bd_s["bike"]["sclad"]) ? 
+echo ($bd_simple["bear"]["number"]<=$bd_simple["bear"]["storage"]
+    && $bd_simple["jacket"]["number"]<=$bd_simple["jacket"]["storage"]
+    && $bd_simple["bike"]["number"]<=$bd_simple["bike"]["storage"]) ? 
 '<font color="blue">Все ок, всего хватает!</font><br>' :
 '<font color="red">Вы заказали слишком большое количество товаров:</font>';
 echo '<br>';
-if ($bd_s["bear"]["number"]>$bd_s["bear"]["sclad"]){
+if ($bd_simple["bear"]["number"]>$bd_simple["bear"]["storage"]){
     echo '<font color="red">-Мишек столько нет в наличии!</font><br>';
 }
-if ($bd_s["jacket"]["number"]>$bd_s["jacket"]["sclad"]){
+if ($bd_simple["jacket"]["number"]>$bd_simple["jacket"]["storage"]){
     echo '<font color="red">-Курток столько нет в наличии!</font><br>';
 }
-if ($bd_s["bike"]["number"]>$bd_s["bike"]["sclad"]){
+if ($bd_simple["bike"]["number"]>$bd_simple["bike"]["storage"]){
     echo '<font color="red">-Великов столько нет в наличии!</font><br>';
 }
 echo '<br>';
 
 //ниже использую switch, если требуется по ТЗ, можно раскомментить
 
-//switch ($bd_s["bear"]["sclad"]) {
+//switch ($bd_simple["bear"]["storage"]) {
 //    case 0:
 //        echo 'Мишек нет на складе, закрытая позиция';
 //
@@ -212,7 +212,7 @@ echo '<br>';
 //        break;
 //}
 //echo '<br>';
-//switch ($bd_s["jacket"]["sclad"]) {
+//switch ($bd_simple["jacket"]["storage"]) {
 //    case 0:
 //        echo 'Курток нет на складе, закрытая позиция';
 //
@@ -223,7 +223,7 @@ echo '<br>';
 //        break;
 //}
 //echo '<br>';
-//switch ($bd_s["bike"]["sclad"]) {
+//switch ($bd_simple["bike"]["storage"]) {
 //    case 0:
 //        echo 'Великов нет на складе, закрытая позиция';
 //
